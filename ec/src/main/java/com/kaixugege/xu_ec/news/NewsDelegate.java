15f82ba3ae25da14d0629cb990dba380632732fc
kaixu <604312074@qq.com>
2019-04-29 23:01:19 +0800

    change some tab!

--- ec/src/main/java/com/kaixugege/xu_ec/news/NewsDelegate.java
+++ ec/src/main/java/com/kaixugege/xu_ec/news/NewsDelegate.java
@@ -1,18 +1,13 @@
 package com.kaixugege.xu_ec.news;
 
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.kaixugege.xu.core.ui.fragments.BaseDelegate;
+import com.kaixugege.xu.tablayout.tablayout.SlidingTabLayout;
 import com.kaixugege.xu_ec.R;
-
-import org.w3c.dom.Text;
-
-import static android.support.design.widget.TabLayout.MODE_SCROLLABLE;
 
 /**
  * @Author: KaixuGege
@@ -26,7 +21,7 @@ public class NewsDelegate extends BaseDelegate {
 
     ViewPager viewPager;
     VpAdapter vpAdapter;
-    TabLayout tab;
+    SlidingTabLayout tab;
     ImageView mAddTab;
 
     @Override
@@ -41,23 +36,23 @@ public class NewsDelegate extends BaseDelegate {
 
         viewPager = rootView.findViewById(R.id.news_vp);
         tab = rootView.findViewById(R.id.tabContent);
-        mAddTab = rootView.findViewById(R.id.content_add_tab);
-
+//        mAddTab = rootView.findViewById(R.id.content_add_tab);
+        mAddTab = ViewFindUtils.find(rootView,R.id.content_add_tab);
         vpAdapter = new VpAdapter(getFragmentManager(), Helper.initChannelData(), Helper.initFragmentData());
         viewPager.setAdapter(vpAdapter);
         viewPager.setOffscreenPageLimit(Helper.initFragmentData().size());
 
-        tab.setTabMode(MODE_SCROLLABLE);
-        tab.setupWithViewPager(viewPager);
+//        tab.setTabMode(MODE_SCROLLABLE);
+//        tab.setupWithViewPager(viewPager);
 
 
-        tab.setSelectedTabIndicatorColor(getResources().getColor(R.color.red));
+//        tab.setSelectedTabIndicatorColor(getResources().getColor(R.color.red));
         tab.setMinimumWidth(15);
-        tab.setTabTextColors(getResources().getColor(R.color.design_default_color_primary),getResources().getColor(R.color.red));
+//        tab.setTabTextColors(getResources().getColor(R.color.design_default_color_primary),getResources().getColor(R.color.red));
         int i = 0;
-        for (Channel channel : Helper.initChannelData()) {
-            tab.getTabAt(i).setText("是"+i);
-            tab.getTabAt(i).setCustomView(R.layout.tab_news);
+//        for (Channel channel : Helper.initChannelData()) {
+//            tab.getTabAt(i).setText("是"+i);
+//            tab.getTabAt(i).setCustomView(R.layout.tab_news);
 //            View view1 = tab.getTabAt(i).getCustomView();
 //            if (view1!= null){
 ////                view1.setBackground(new ProxyDrawable(view1));
@@ -77,9 +72,9 @@ public class NewsDelegate extends BaseDelegate {
 //            View view = tab.getTabAt(i).getCustomView();
 //            TextView textView = view.findViewById(R.id.tab_news_txt);
 //            textView.setText(channel.getTitle()+" !!");
-            i++;
-        }
-        tab.setTabIndicatorFullWidth(false);
+//            i++;
+//        }
+//        tab.setTabIndicatorFullWidth(false);
         return null;
     }
 
